3be0c326e87c0ddb707c3a5fcf03dc97ba7080a9
Alex Sova <foxeyes.ml@gmail.com>
2022-06-13 17:03:21 +0300

    build

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,5 @@
+const FORM_HANDLER_URL = 'https://script.google.com/macros/s/AKfycbycXMpObbm9sOn-kO21x6cqFtjEO1-kR0yndp9bgOWE356FIDndhFeinCxBcJvjQz0Ulg/exec';
+
 function init() {
   let menuLinks = [...document.querySelectorAll('nav a')];
   let sections = [...document.querySelectorAll('section')];
@@ -6,9 +8,11 @@ function init() {
       entries[0].target.setAttribute('active', '');
       let sId = entries[0].target.getAttribute('id');
       menuLinks.forEach((el) => {
-        let aId = el.getAttribute('href').replace('#', '');
+        let hash = el.getAttribute('href');
+        let aId = hash.replace('#', '');
         if (sId === aId) {
           el.setAttribute('active', '');
+          window.history.pushState(null, '', hash);
         } else {
           el.removeAttribute('active');
         }
@@ -21,7 +25,26 @@ function init() {
     observer.observe(section);
   });
   
-}
+  let form = document.querySelector('form');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    window.location.hash = '#closed';
+    let data = new FormData(form);
+    window.fetch(form.action, {
+      method: 'POST',
+      body: data,
+    }).then(async (resp) => {
+      let answer = await resp.json();
+      if (answer?.result === 'success') {
+        window.location.hash = '#invited';
+      }
+    }).catch((err) => {
+      window.location.hash = '#error';
+      console.log(err);
+    });
+  });
+};
+
 window.onload = () => {
   init();
 };
